bdc37bc2536020e36a232599c2c391c796eba1e0
Junaid Haque <junaid132boss@gmail.com>
2025-10-30 02:39:08 -0700

    Refactor Hero component: enhance animation logic for header and image, and improve structure for better readability

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -10,6 +10,7 @@ import { stagger } from "motion";
 
 const Hero: FC = () => {
   const [heroH1Scope, heroH1Animate] = useAnimate();
+  const [imageScope, imageAnimate] = useAnimate();
 
   useEffect(() => {
     new SplitType(heroH1Scope.current, {
@@ -17,11 +18,14 @@ const Hero: FC = () => {
       tagName: "span",
     });
 
+    imageAnimate(imageScope.current, { opacity: 1, translateY: 0 }, { duration: 0.7 })
+
     heroH1Animate(
       heroH1Scope.current.querySelectorAll(".word"),
       { transform: "translateY(0)" },
       { duration: 0.5, delay: stagger(0.2) }
     );
+    
   }, []);
 
   return (
@@ -40,58 +44,29 @@ const Hero: FC = () => {
               {/* Creating experiences that matter through thoughtful code */}
               Creating meaningful experiences through crafted code
             </motion.h1>
-            <div className="flex flex-col md:flex-row md:items-center items-start mt-10 gap-6">
+            <motion.div 
+              initial={{opacity: 0}} 
+              animate={{opacity: 1}} 
+              transition={{
+              duration: 0.5,
+              delay: 1.2, // This will start after the h1 animation
+              }}
+              className="flex flex-col md:flex-row md:items-center items-start mt-10 gap-6"
+            >
+              <motion.div
+              initial={{opacity: 0, y: 20}}
+              animate={{opacity: 1, y: 0}}
+              transition={{
+                duration: 0.5,
+                delay: 1.4,
+              }}
+              >
               <Button
                 className="hidden md:inline-flex"
                 varient="secondary"
                 iconAfter={
-          <div className="overflow-hidden size-6 relative z-0">
-            <div className="size-6 relative z-0 flex items-center justify-center group-hover/button:translate-y-0 -translate-y-6 transition-all duration-300">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        className="size-5"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
-                        />
-                      </svg>
-                    </div>
-                    <div className="size-6 relative z-0 flex items-center justify-center group-hover/button:translate-y-0 -translate-y-6 transition-all duration-300">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        className="size-5"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
-                        />
-                      </svg>
-                    </div>
-                  </div>
-                }
-              >
-                View my work
-              </Button>
-              <a href="/resume.pdf" download>
-                <Button varient="primary" className="inline-flex md:hidden">
-                  Download CV
-                </Button>
-              </a>
-              <Button
-                className="inline-flex md:hidden"
-                varient="text"
-                iconAfter={
+                <div className="overflow-hidden size-6 relative z-0">
+                  <div className="size-6 relative z-0 flex items-center justify-center group-hover/button:translate-y-0 -translate-y-6 transition-all duration-300">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -101,30 +76,103 @@ const Hero: FC = () => {
                     className="size-5"
                   >
                     <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
                     />
                   </svg>
+                  </div>
+                  <div className="size-6 relative z-0 flex items-center justify-center group-hover/button:translate-y-0 -translate-y-6 transition-all duration-300">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-5"
+                  >
+                    <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
+                    />
+                  </svg>
+                  </div>
+                </div>
+                }
+              >
+                View my work
+              </Button>
+              </motion.div>
+              <motion.div
+              initial={{opacity: 0, y: 20}}
+              animate={{opacity: 1, y: 0}}
+              transition={{
+                duration: 0.5,
+                delay: 1.6,
+              }}
+              >
+              <a href="/resume.pdf" download>
+                <Button varient="primary" className="inline-flex md:hidden">
+                Download CV
+                </Button>
+              </a>
+              </motion.div>
+              <motion.div
+              initial={{opacity: 0, y: 20}}
+              animate={{opacity: 1, y: 0}}
+              transition={{
+                duration: 0.5,
+                delay: 1.8,
+              }}
+              >
+              <Button
+                className="inline-flex md:hidden"
+                varient="text"
+                iconAfter={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="size-5"
+                >
+                  <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
+                  />
+                </svg>
                 }
               >
                 Let&apos;s Talk
               </Button>
+              </motion.div>
+              <motion.div
+              initial={{opacity: 0, y: 20}}
+              animate={{opacity: 1, y: 0}}
+              transition={{
+                duration: 0.5,
+                delay: 1.8,
+              }}
+              >
               <Button className="hidden md:inline-flex" varient="text">
                 Let&apos;s Talk
               </Button>
-            </div>
+              </motion.div>
+            </motion.div>
           </div>
         </div>
         <div className="md:col-span-5">
-          <div className="mt-20 md:mt-0 md:h-full xl:h-screen">
+          <motion.div ref={imageScope} initial={{ opacity: 0, translateY: -25 }} className="mt-20 md:mt-0 md:h-full xl:h-screen">
             <Image
               src={heroImage}
               alt="My Image"
               priority
               className="size-full object-cover"
             />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
