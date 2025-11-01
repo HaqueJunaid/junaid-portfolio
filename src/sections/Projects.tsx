b85a84ebba47ba7bdd4e59ad5955930a72188770
Junaid Haque <junaid132boss@gmail.com>
2025-10-31 23:39:54 -0700

    Refactor Projects component: enhance project item layout and add hover effects for improved interactivity

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import placeholder from "@/assets/images/gato-papu.gif";
 import Image from "next/image";
+import { div } from "motion/react-client";
 
 /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 const projects = [
@@ -32,14 +33,15 @@ const Projects: FC = () => {
       <div className="container">
         <h2 className="text-4xl md:text-7xl lg:text-8xl xl:text-9xl">Selected Works</h2>
         <div className="mt-8 md:mt-10 lg:mt-12 xl:mt-16">
-          {projects.map(({ name, image }) => 
-              <a href="#" key={name} className="flex py-6 md:py-8 lg:py-10 last:border-b border-t border-stone-400">
-                <div className="w-full">
-                  <div className="w-full md:hidden aspect-video">
+          {projects.map(({ name, image }) => (
+              <a href="#" key={name} className="flex group/projects relative py-6 md:py-8 lg:py-10 last:border-b border-t border-stone-400">
+                <div className="w-full h-0 bg-stone-300 absolute bottom-0 left-0 group-hover/projects:h-full transition-all duration-300"></div>
+                <div className="w-full relative">
+                  <div className="w-full md:absolute md:opacity-0 md:w-64 lg:w-96 md:-top-1/2 md:-translate-y-[25%] lg:-translate-y-1/3 md:right-12 lg:right-16 xl:right-20 md:group-hover/projects:block md:group-hover/projects:opacity-100 transition-all duration-500 aspect-video z-10">
                     <Image src={image} alt={name} className="size-full object-cover" />
                   </div>
                   <div className="mt-8 md:mt-0 flex justify-between items-center">
-                    <h3 className="text-2xl md:text-3xl lg:text-4xl">{name}</h3>
+                    <h3 className="text-2xl md:text-3xl lg:text-4xl group-hover/projects:pl-4 md:group-hover/projects:pl-12 transition-all duration-300">{name}</h3>
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
@@ -57,7 +59,7 @@ const Projects: FC = () => {
                   </div>
                 </div>
               </a>
-          
+          )
           )}
         </div>
       </div>
