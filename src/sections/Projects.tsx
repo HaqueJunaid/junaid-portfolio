88b39f0be495522181cb18fe6336eb7ace454469
Junaid Haque <junaid132boss@gmail.com>
2025-12-21 03:29:42 -0800

    Second project added

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -2,16 +2,18 @@ import { FC } from "react";
 import placeholder from "@/assets/images/gato-papu.gif";
 import Image from "next/image";
 import Wresumes from "@/assets/images/WResumes.png"
+import Nooter from "@/assets/images/Nooter.png"
 
 const projects = [
   {
-    name: "WResumes",
+    name: "WResumes (AI resume analyzer)",
     image: Wresumes,
     url: "https://wresumes.vercel.app"
   },
   {
-    name: "Yet to select",
-    image: placeholder,
+    name: "Nooter (Advance notes app)",
+    image: Nooter,
+    url: "https//nooter.vercel.app"
   },
   {
     name: "Yet to select",
@@ -38,7 +40,7 @@ const Projects: FC = () => {
                 <div className="w-full h-0 bg-stone-300 absolute bottom-0 left-0 group-hover/projects:h-full transition-all duration-300"></div>
                 <div className="w-full relative">
                   <div className="w-full md:absolute md:opacity-0 md:w-64 lg:w-96 md:-top-1/2 md:-translate-y-[25%] lg:-translate-y-1/3 md:right-12 lg:right-16 xl:right-20 md:group-hover/projects:block md:group-hover/projects:opacity-100 transition-all duration-500 aspect-video z-10">
-                    <Image src={image} alt={name} className="size-full object-cover" />
+                    <Image src={image} alt={name} className="size-full object-cover object-top" />
                   </div>
                   <div className="mt-8 md:mt-0 flex justify-between items-center">
                     <h3 className="text-2xl md:text-3xl lg:text-4xl group-hover/projects:pl-4 md:group-hover/projects:pl-12 transition-all duration-300">{name}</h3>
